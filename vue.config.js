79df72ce42a053a2f95ec27dd3565aa30af5d8eb
Martynas Skrebė <martynaskrebe@gmail.com>
2021-03-07 22:32:04 +0200

    added vue config

---
+++ vue.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'renderer': path.join(__dirname, './src/renderer'),
+        'shared': path.join(__dirname, './src/shared')
+      }
+    },
+  },
+  pluginOptions: {
+    electronBuilder: {
+      removeElectronJunk: false,
+      chainWebpackMainProcess: (config) => {
+        config.resolve
+              .alias
+              .set('main', path.resolve(__dirname, './src/main'))
+              .set('shared', path.resolve(__dirname, './src/shared'))
+      },
+      mainProcessFile: 'src/main/Background.ts',
+      mainProcessWatch: [
+        'src/main/**/*'
+      ],
+      rendererProcessFile: 'src/renderer/main.ts',
+      externals: ['tray-window-state-manager'],
+      builderOptions: {
+        extraResources: [
+          {
+            "from": "src/assets",
+            "to": "resources",
+            "filter": [
+              "**/*"
+            ]
+          }
+        ]
+      }
+    }
+  }
+}
