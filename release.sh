5cc3708bbd8918707e332e8391aa35e793bddd40
Martynas <martynaskrebe@gmail.com>
2021-04-06 15:18:49 +0300

    updated release script

--- release.sh
+++ release.sh
@@ -2,17 +2,13 @@
 
 version=${1?Error: Pass new version}
 currentVersion=$(node -p "require('./package.json').version")
-branch=$(git branch --show-current)
 
 if [[ "$version" == "$currentVersion" ]]
 then
-  if [[ "$branch" == "master" ]]
-  then
-    git merge origin "$version" --allow-unrelated-histories
-    git commit -am v"$version"
-    git tag v"$version"
-    git push -u origin master && git push -u origin master --tags
-  fi
+  git merge origin "$version" --allow-unrelated-histories
+  git commit -am v"$version"
+  git tag v"$version"
+  git push -u origin master && git push -u origin master --tags
 else
   echo "Mismatch between provided versions. Current version: "$currentVersion", release version: "$version
 fi
